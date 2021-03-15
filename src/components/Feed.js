40dcd27ed557963ecc75e0bbcaeb0d3be852c94d
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-03-15 23:50:42 +0100

    input options add styles

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "../styles/Feed.css";
 import CreateIcon from "@material-ui/icons/Create";
+import InputOption from "./InputOption";
+import PhotoSizeSelectActualIcon from "@material-ui/icons/PhotoSizeSelectActual";
+import VideogameAssetIcon from "@material-ui/icons/VideogameAsset";
+import EventIcon from "@material-ui/icons/Event";
+import DescriptionIcon from "@material-ui/icons/Description";
 
 function Feed() {
   return (
@@ -12,6 +17,20 @@ function Feed() {
             <input type="text" />
             <button type="submit">Send</button>
           </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption
+            Icon={PhotoSizeSelectActualIcon}
+            title="Photo"
+            color="#7085f9"
+          />
+          <InputOption
+            Icon={VideogameAssetIcon}
+            title="Video"
+            color="#e7a33e"
+          />
+          <InputOption Icon={EventIcon} title="Event" color="#c0cbcd" />
+          <InputOption Icon={DescriptionIcon} title="Write" color="#7fc15e" />
         </div>
       </div>
     </div>
